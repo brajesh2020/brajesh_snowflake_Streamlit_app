2f03647ce723a15bab936c311a9b8f5d388c0a63
brajesh2020 <brajesh@live.com>
2023-12-24 21:34:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-#import snowflake.connector
-import snowflake-connector-python.connector
+import snowflake.connector #import snowflake-connector-python.connector
 my_cnx = snowflake-connector-python.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
